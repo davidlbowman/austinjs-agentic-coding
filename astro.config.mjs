55557395a1d1a36ca00cb3c4f370f85984f5eb90
David L. Bowman <david@theinnovationlab.dev>
2025-07-13 07:06:12 -0500

    mcp

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import mcp from "astro-mcp";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react()],
+    integrations: [react(), mcp()],
 
-	vite: {
-		plugins: [tailwindcss()],
-	},
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
